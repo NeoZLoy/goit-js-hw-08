f5f8087fcf97bfc7c4db14b397b030027373dfa7
NeoZLoy <123673700+NeoZLoy@users.noreply.github.com>
2023-06-21 17:11:44 +0200

    task 3 update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,7 @@
 import throttle from 'lodash.throttle';
 import onFormSubmit from './on-form-submit';
 import onFormInput from './on-form-input';
-import formOutput from './form-output';
+// import formOutput from './form-output';
 
 const inputMailEl = document.querySelector('.feedback-form input');
 const inputMessageEl = document.querySelector('.feedback-form textarea');
@@ -21,3 +21,17 @@ formOutput();
 formEl.addEventListener('submit', onFormSubmit);
 
 formEl.addEventListener('input', throttle(onFormInput, 1000));
+
+function formOutput() {
+  const savedForm = localStorage.getItem('feedback-form-state');
+
+  const parsedForm = JSON.parse(savedForm);
+
+  if (parsedForm) {
+    formData = parsedForm;
+
+    inputMailEl.value = parsedForm.email || '';
+
+    inputMessageEl.value = parsedForm.message || '';
+  }
+}
--- src/js/form-output.js
+++ src/js/form-output.js
@@ -1,15 +1,3 @@
-import { inputMailEl, inputMessageEl, formData } from './03-feedback';
+// import { inputMailEl, inputMessageEl, formData } from './03-feedback';
 
-export default function formOutput() {
-  const savedForm = localStorage.getItem('feedback-form-state');
-
-  const parsedForm = JSON.parse(savedForm);
-
-  if (parsedForm) {
-    formData = parsedForm;
-
-    inputMailEl.value = parsedForm.email || '';
-
-    inputMessageEl.value = parsedForm.message || '';
-  }
-}
+// export default
