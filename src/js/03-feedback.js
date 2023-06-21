0a3d8feb8cfbc292fae352911941c1b71dbee7ea
NeoZLoy <123673700+NeoZLoy@users.noreply.github.com>
2023-06-21 17:15:16 +0200

    update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,6 @@
 import throttle from 'lodash.throttle';
 import onFormSubmit from './on-form-submit';
 import onFormInput from './on-form-input';
-// import formOutput from './form-output';
 
 const inputMailEl = document.querySelector('.feedback-form input');
 const inputMessageEl = document.querySelector('.feedback-form textarea');
--- src/js/form-output.js
+++
@@ -1,3 +0,0 @@
-// import { inputMailEl, inputMessageEl, formData } from './03-feedback';
-
-// export default
